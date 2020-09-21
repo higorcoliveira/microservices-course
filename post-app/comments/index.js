7f75772661842b60cb23eae4d46f3b0486e90bfd
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-09-21 15:39:12 -0300

    Readme, serviço moderação e ajustes no eventbus

--- post-app/comments/index.js
+++ post-app/comments/index.js
@@ -22,6 +22,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   comments.push({
     id: commentId,
     content,
+    status: "pending",
   });
 
   commentsByPostId[req.params.id] = comments;
@@ -31,7 +32,8 @@ app.post("/posts/:id/comments", async (req, res) => {
     data: {
       id: commentId,
       content,
-      postId: req.params.id
+      postId: req.params.id,
+      status: "pending",
     },
   });
 
@@ -39,7 +41,27 @@ app.post("/posts/:id/comments", async (req, res) => {
 });
 
 app.post("/events", async (req, res) => {
-  console.log('Received Event', req.body.type);
+  console.log("Received Event", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    // update a comment
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find(comment => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id, postId, status, content
+      }
+    });
+  }
 
   res.send({});
 });
