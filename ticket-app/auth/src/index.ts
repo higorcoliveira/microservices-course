dfc99109a935fed9809533359b103ff156cf4962
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-11-09 18:55:03 -0300

    Criação de Error Handler

--- ticket-app/auth/src/index.ts
+++ ticket-app/auth/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import "express-async-errors";
 import { json } from "body-parser";
 
 import { currentUserRouter } from "./routes/currentUser";
 import { signinRouter } from "./routes/signin";
 import { signupRouter } from "./routes/signup";
 import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -13,6 +16,12 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log(`Listening on ${3000}!!!`);
