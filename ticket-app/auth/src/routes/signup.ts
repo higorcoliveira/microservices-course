3ee08d915125e40fabd74117c77a0ccb677a9442
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-11-20 12:11:03 -0300

    Início da autenticação com JWT

--- ticket-app/auth/src/routes/signup.ts
+++ ticket-app/auth/src/routes/signup.ts
@@ -1,21 +1,22 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
-import { User } from '../models/user';
+import { User } from "../models/user";
 import { RequestValidationError } from "../errors/request-validation-errors";
 import { BadRequestError } from "../errors/bad-request-error";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.post("/api/users/signup", 
+router.post(
+  "/api/users/signup",
   [
-    body('email')
-     .isEmail()
-     .withMessage('Email must be valid'),
-    body('password')
-        .trim()
-        .isLength({min: 4, max: 20})
-        .withMessage('Password must be between 4 and 20 characters')
-  ], async (req: Request, res: Response) => {
+    body("email").isEmail().withMessage("Email must be valid"),
+    body("password")
+      .trim()
+      .isLength({ min: 4, max: 20 })
+      .withMessage("Password must be between 4 and 20 characters"),
+  ],
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       throw new RequestValidationError(errors.array());
@@ -25,13 +26,25 @@ router.post("/api/users/signup",
     const existingUser = await User.findOne({ email });
 
     if (existingUser) {
-      throw new BadRequestError('Email in use');
+      throw new BadRequestError("Email in use");
     }
 
     const user = User.build({ email, password });
     await user.save();
 
+    // Generate JWT
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email,
+      }, "asdf");
+
+    // Store it on session object
+    req.session = {
+      jwt: userJwt
+    };
+
     res.status(201).send(user);
-});
+  }
+);
 
 export { router as signupRouter };
