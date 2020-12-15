d4ae991e9238a61484fe949bf50e01220230ea75
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-12-15 17:30:57 -0300

    Infraestrutura de testes e desenvolvimento de alguns testes

---
+++ ticket-app/auth/src/routes/__test__/signin.test.ts
@@ -0,0 +1,70 @@
+import request from "supertest";
+import { app } from "../../app";
+
+it("fails when a email that does not exist is supplied", async () => {
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with an invalid email", async () => {
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "testtest.com",
+      password: "password",
+    })
+    .expect(400);
+});
+
+it("returns a 400 with empty password", async () => {
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "",
+    })
+    .expect(400);
+});
+
+it("fails when an incorrect password", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "123456",
+    })
+    .expect(201);
+
+  return request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "4567890",
+    })
+    .expect(400);
+});
+
+it("response with a cookie with valid credentials", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "123456",
+    })
+    .expect(201);
+
+  const response = await request(app)
+    .post("/api/users/signin")
+    .send({
+      email: "test@test.com",
+      password: "123456",
+    })
+    .expect(200);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
