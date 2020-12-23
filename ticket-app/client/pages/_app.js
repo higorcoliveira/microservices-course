eb0d13f1fe80fa55df771329e4673f6897bdc3e4
Higor Oliveira <higor.oliveira@grupomult.com.br>
2020-12-23 12:43:04 -0300

    Header e ajustes no fetch de dados pelo getInitialProps

--- ticket-app/client/pages/_app.js
+++ ticket-app/client/pages/_app.js
@@ -1,6 +1,28 @@
 import "bootstrap/dist/css/bootstrap.css";
+import buildClient from "../api/build-client";
+import Header from '../component/header';
 
 // custom global app component wrapper
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+// get data on rendering process SSR
+AppComponent.getInitialProps = async (appContext) => {
+  const client = buildClient(appContext.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+
+  return { pageProps, ...data };
+};
+
+export default AppComponent;
